080aa218f58b977868dd6302bd0cb2959798d1f6
mosharif30 <sharifkhani.mr@gmail.com>
2022-04-22 01:38:18 +0430

    create about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,31 @@
+import Button from "../Button/Button";
 import Layout from "../Layout/Layout";
+import "./About.css";
+import laptop from "./laptop.jpg";
 
 const About = () => {
   return (
     <>
-      <Layout>About</Layout>
+      <Layout>
+        <div className="about">
+          <div className="pic">
+            <img className="laptop" src={laptop} alt="" srcset="" />
+          </div>
+          <div className="text">
+            <h1 className="header">I am junior developer</h1>
+            <h3 className="header">Bachelor in computer science</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              facere maiores reiciendis quis harum dolores recusandae ea fugit,
+              numquam ex voluptatum molestias asperiores quisquam quas hic velit
+              voluptas minus ipsum beatae sapiente, distinctio vitae. Voluptate
+              odio nesciunt mollitia deserunt? Maiores temporibus perferendis
+              laudantium vel illum in tempora dolorum pariatur enim.
+            </p>
+            <Button>Download Resume</Button>
+          </div>
+        </div>
+      </Layout>
     </>
   );
 };
