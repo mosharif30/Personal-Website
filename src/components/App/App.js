5ecf164f7278c82e01534a7c70be8a321d3f2b50
mosharif30 <sharifkhani.mr@gmail.com>
2022-04-22 13:30:58 +0430

    add bootstrapp to project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import Contact from "../Contact/Contact";
 import Portfolio from "../Portfolio/Portfolio";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
